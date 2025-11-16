c2a42d8c3abf68263c4d22c149a977bded7e9278
Marheen <rabadija73@gmail.com>
2025-11-16 17:55:52 +0100

    setup Vue.js for frontend

--- start.sh
+++ start.sh
@@ -1,4 +1,10 @@
 docker compose down -v #shuts down all containers and deletes all data
 docker compose up -d --build #rebuilds backend and starts all containers
-docker logs backend1 -f #monitors backend logs
+
+curl http://localhost # test frontend
+
+#monitors backend logs
+docker logs backend1 -f
 docker logs backend2 -f
+# check frontend logs
+docker logs frontend
